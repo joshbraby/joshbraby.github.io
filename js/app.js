8bbdeab65a5c3cdd13968932ea89544a91b4c5cf
braby21 <braby21@gmail.com>
2016-05-01 15:34:33 -0700

    adds JS animation

--- js/app.js
+++ js/app.js
@@ -3,4 +3,15 @@ $(function () {
     var currentYear = new Date();
     $('#footer-date').html("&copy;" + currentYear.getFullYear() + " Josh Braby");
 
+    $('#initial-hello').animate({
+        opacity: 1
+      },1000, function() {
+        // Animation complete.
+        $('#initial-enter').animate({
+            opacity: 1
+          }, 500, function() {
+            //animation complete.
+        });
+    });
+
 }); //end document object function
